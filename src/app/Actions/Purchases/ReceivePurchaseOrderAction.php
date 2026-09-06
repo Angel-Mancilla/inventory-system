dd6f112f742f107a4dc4a1c9016bff443d657676
Angel Mancilla <angeldft27@gmail.com>
2026-09-06 01:31:38 -0600

    feat: agrega estructura inicial para controladores, actions, requests, resources, policies y repositorios

---
+++ src/app/Actions/Purchases/ReceivePurchaseOrderAction.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Actions\Purchases;
+
+class ReceivePurchaseOrderAction
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Invoke the class instance.
+     */
+    public function __invoke(): void
+    {
+        //
+    }
+}
---
+++ src/app/DTOs/Sales/CreateSaleData.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\DTOs\Sales;
+
+class CreateSaleData
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+}
