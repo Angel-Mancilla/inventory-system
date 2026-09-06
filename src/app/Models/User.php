154bc39a3321c04390d8d8b4c342d1ec42f00816
Angel Mancilla <angeldft27@gmail.com>
2026-09-06 00:42:33 -0600

    Añadir el trait `SoftDeletes` a los modelos `Article`, `Brand`, `Category`, `ProductModel` y `User`; actualizar la migración de usuarios para incluir el borrado lógico.

--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -7,6 +7,7 @@ use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Spatie\Permission\Traits\HasRoles; 
@@ -30,7 +31,7 @@ use Illuminate\Support\Carbon;
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
-    use HasFactory, Notifiable, HasRoles;
+    use HasFactory, Notifiable, HasRoles, SoftDeletes;
 
     /**
      * Get the attributes that should be cast.
